917af0b92d83a4f1c49d3ace84909dd9d7adb195
xuan dong <vuxuandong.004@gmail.com>
2024-09-29 12:17:19 +0700

    Update sidebar

--- public/JS/index.js
+++ public/JS/index.js
@@ -10,6 +10,20 @@ const loggedIn = document.getElementById('loggedIn');
 const notLoggedIn = document.getElementById('notLoggedIn');
 // Bắt sự kiện khi người dùng đăng nhập
 
+// Chọn item trong sidebar
+const navMenu = document.querySelectorAll('#nav-menu');
+navMenu.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.classList.add('active');
+        item.classList.remove('text-dark');
+    });
+    item.addEventListener('mouseleave', () => {
+        item.classList.remove('active');
+        item.classList.add('text-dark');
+    });
+})
+// Chọn item trong sidebar
+
 // Login event is activated by click button "Login"
 login.addEventListener('click', () => {
     loginPage.classList.remove('d-none');
@@ -31,6 +45,13 @@ closeIcons.forEach(icon => {
     })
 });
 
-bxMenu.addEventListener('click', () => {
+bxMenu.addEventListener('click', (event) => {
     sidebar.classList.toggle('d-none');
+});
+
+// Delete menu event when click anywhere except bxMenu
+document.body.addEventListener('click', (event) => {
+    if(event.target !== bxMenu){
+        sidebar.classList.add('d-none');
+    }
 });
